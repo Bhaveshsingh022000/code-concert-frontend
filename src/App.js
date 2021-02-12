26e9abab143f002c0fdacac886312d48b33a9ce4
Bhavesh <bhaveshsingh022000@hotmail.com>
2021-02-12 23:06:22 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+import './App.css';
+import TextEditor from './Containers/textEditior';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import Landing from './Containers/Landing/Landing';
+
+class App extends Component {
+  render() {
+    let routes = (
+      <Switch>
+          <Route path="/" exact component={Landing} />
+          <Redirect to="/" />
+        </Switch>
+    );
+    if(this.props.isAuth){
+      routes = (
+        <Switch>
+          <Route path="/editor" component={TextEditor} />
+          <Route path="/" exact component={Landing} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+    
+    return (  
+      <div className="App">
+        {routes}
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state)=>{
+  return{
+    isAuth: state.auth.isAuth
+  }
+}
+
+export default connect(mapStateToProps,null)(withRouter(App));
